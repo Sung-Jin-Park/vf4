e2ecd1c125052061f8d4beae94c2204f046a40b4
Sung-Jin-Park <skyplusp128@gmail.com>
2021-08-10 10:07:10 +0900

    app bar, footer

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,23 @@
 module.exports = {
   root: true,
+
   env: {
     node: true
   },
+
   extends: [
     'plugin:vue/essential',
-    '@vue/standard'
+    '@vue/standard',
+    'eslint:recommended'
   ],
+
   parserOptions: {
     parser: 'babel-eslint'
   },
+
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    // 'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    // 'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
   }
+  
 }
